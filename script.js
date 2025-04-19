060acc5a73aaf68744c520dfba4afe14400d930b
Subham Biswas <subhambiswas.vawsum@gmail.com>
2025-04-19 22:25:54 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const API_KEY = "b69390dbb63141bb9a3488b23892c76b";
-const url = "https://newsapi.org/v2/everything?q=";
+// const API_KEY = "b69390dbb63141bb9a3488b23892c76b";
+// const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
 
 async function fetchNews (query) {
-    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    // const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    const res = await fetch('https://newsapi.org/v2/everything?q=India&apiKey=b69390dbb63141bb9a3488b23892c76b');
     const data = await res.json();
     console.log(data)
     bindData(data.articles)
